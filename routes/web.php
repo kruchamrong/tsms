6487ff943b0b082ebc52b65346f7c233e7105d17
kruchamrong <kruchamrong@gmail.com>
2026-07-14 22:20:21 +0700

    Remove Delete Account feature for safety

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
     Route::middleware('is_school_admin')->group(function () {
         Route::get('/teachers/template', [TeacherController::class, 'downloadTemplate'])->name('teachers.template');

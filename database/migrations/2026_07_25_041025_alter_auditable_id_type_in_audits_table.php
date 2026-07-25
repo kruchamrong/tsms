cfe48dce40fc9913188a487916019da4618b0c47
kruchamrong <kruchamrong@gmail.com>
2026-07-25 11:15:27 +0700

    Update migration to drop index before altering column type

--- database/migrations/2026_07_25_041025_alter_auditable_id_type_in_audits_table.php
+++ database/migrations/2026_07_25_041025_alter_auditable_id_type_in_audits_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::table('audits', function (Blueprint $table) {
+            $table->dropIndex('audits_auditable_type_auditable_id_index');
+        });
+
         // Use raw SQL for Postgres to allow type casting from bigint to varchar
         DB::statement('ALTER TABLE audits ALTER COLUMN auditable_id TYPE VARCHAR(36) USING auditable_id::VARCHAR(36)');
+
+        Schema::table('audits', function (Blueprint $table) {
+            $table->index(['auditable_type', 'auditable_id'], 'audits_auditable_type_auditable_id_index');
+        });
     }
 
     /**
